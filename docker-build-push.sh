583229935b6458c89ca92db23382063b153b4e20
Ferran Maylinch <ferran@yandex-team.ru>
2024-12-07 17:45:14 +0100

    replace docker by podman

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,3 +1,3 @@
-docker build . -t fmaylinch/flashcards --platform 'linux/amd64'
-docker push fmaylinch/flashcards
-# docker run -p 3001:3001 -d fmaylinch/flashcards
+podman build . -t fmaylinch/flashcards --platform 'linux/amd64'
+podman push fmaylinch/flashcards
+# podman run -p 3001:3001 -d fmaylinch/flashcards
